96fda71480105193b5c7035edeb23994cbc03fd4
ajith <ajithvonline@gmail.com>
2020-07-25 08:15:09 +0530

    part 5

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 3080
 
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 app.use(express.static(__dirname+'/public'))
 
+var waitingQue=[]
+var rooms=[]
+var roomId=''
 
-app.listen(port, () => console.log(`Chat app listening at http://localhost:${port}`))
+io.on('connection',socket=>{
+    console.log('a user connected')
+
+    socket.on('login',(res)=>{
+     
+        if(waitingQue.length>0){
+
+            popedUser=waitingQue.pop()
+
+            roomId=socket.id+'+'+popedUser.socket.id   
+
+            popedUser.socket.join(roomId)
+            socket.join(roomId)
+           
+            rooms[socket.id]=roomId
+            rooms[popedUser.socket.id]=roomId
+
+            socket.emit('Start Chat',{status:'connected'})
+            popedUser.socket.emit('Start Chat',{status:'connected'})
+
+        } else {
+            user={userName:res.userName,socket:socket}
+            waitingQue.push(user)
+        }
+
+    
+
+    })
+   
+    socket.on('disconnect',()=>{
+        waitingQue=[]
+        console.log('User Disconnected')
+    })
+
+})
+
+
+
+
+server.listen(port, () => console.log(`Chat app listening at http://localhost:${port}`))
